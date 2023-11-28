baeb6f86868553afbd569c31a6641f5e8cbd3a3e
srikanthpragada <srikanthpragada@gmail.com>
2023-11-28 18:40:55 +0530

    28-NOV

---
+++ demo/src/gs/EvenOdd.java
@@ -0,0 +1,18 @@
+package gs;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in); // read from keyboard
+
+		System.out.print("Enter number :");
+		int num = s.nextInt(); // read int from keyboard
+
+		if (num % 2 == 0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+}
--- demo/src/gs/Numbers.java
+++ demo/src/gs/Numbers.java
@@ -3,10 +3,16 @@ package gs;
 public class Numbers {
 
 	public static void main(String[] args) {
-		 int i;
-		 
-		 for(i = 1; i <= 10; i ++)
-			 System.out.printf("%d ", i);
+		int i;
+
+		for (i = 1; i <= 10; i++)
+			System.out.printf("%d ", i);
+
+		String s = """
+				ljfkljlsjflkjflkjfsdl
+				sdfjflksdjflsda
+				asldfjsalf
+				""";
 
 	}
 
