e4d24c80b6cbd4b9b13431cf9e147012e531345c
srikanthpragada <srikanthpragada@gmail.com>
2023-12-02 19:16:08 +0530

    02-DEC

---
+++ demo/src/oop1/TestCourse.java
@@ -0,0 +1,40 @@
+package oop1;
+
+class Course {
+	private String title;
+	private int fee, duration;
+
+	// Constructor
+	public Course(String t, int f, int d) {
+		title = t;
+		fee = f;
+		duration = d;
+	}
+
+	public void print() {
+		System.out.println(title);
+		System.out.println(fee);
+		System.out.println(duration);
+	}
+
+	public int getNetFee() {
+		return fee + fee * 12 / 100;
+	}
+	
+	public void setFee(int f) {
+		fee = f;
+	}
+}
+
+public class TestCourse {
+
+	public static void main(String[] args) {
+		Course c = new Course("Java SE", 10000, 30);
+		//Course c2 = new Course();  // calling default constructor 
+		c.setFee(12000);
+		c.print();
+		System.out.println(c.getNetFee());
+
+	}
+
+}
--- demo/src/oop1/TestProduct.java
+++ demo/src/oop1/TestProduct.java
@@ -1,19 +1,13 @@
 package oop1;
 
 public class TestProduct {
-
+	
 	public static void main(String[] args) {
 		 Product p1;  // Object reference 
-		 p1 = new Product();  // Object 
+		 p1 = new Product("iPhone 15", 100000);  // Object 
 		 
 		 // System.out.println(p1.name);
-		 p1.init("iPhone 15", 100000);
 		 p1.print();
-		 
-		 Product p2 = new Product();
-		 p2.init("Samsung Fold", 120000);
-		 p2.print();
-
 	}
 
 }
