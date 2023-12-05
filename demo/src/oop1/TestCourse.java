a3bfe099bc61c4e32be1a62bbb34ba4349464b74
srikanthpragada <srikanthpragada@gmail.com>
2023-12-05 18:54:47 +0530

    05-DEC

--- demo/src/oop1/TestCourse.java
+++ demo/src/oop1/TestCourse.java
@@ -1,38 +1,53 @@
 package oop1;
 
 class Course {
+	// Instance variables
 	private String title;
 	private int fee, duration;
 
+	// static or class variables
+	private static int taxRate = 12;
+
+	public static int getTaxRate() {
+		return Course.taxRate;
+	}
+	
 	// Constructor
-	public Course(String t, int f, int d) {
-		title = t;
-		fee = f;
-		duration = d;
+	public Course(String title, int fee, int duration) {
+		this.title = title;
+		this.fee = fee;
+		this.duration = duration;
 	}
 
 	public void print() {
-		System.out.println(title);
-		System.out.println(fee);
-		System.out.println(duration);
+		System.out.println(this.title);
+		System.out.println(this.fee);
+		System.out.println(this.duration);
 	}
 
 	public int getNetFee() {
-		return fee + fee * 12 / 100;
+		return this.fee + this.fee * Course.taxRate / 100;
 	}
-	
-	public void setFee(int f) {
-		fee = f;
+
+	public void setFee(int fee) {
+		this.fee = fee;
 	}
 }
 
 public class TestCourse {
-
 	public static void main(String[] args) {
+		
 		Course c = new Course("Java SE", 10000, 30);
-		//Course c2 = new Course();  // calling default constructor 
+		// Course c2 = new Course(); // calling default constructor
 		c.setFee(12000);
 		c.print();
 		System.out.println(c.getNetFee());
+		
+		Course c2 = new Course("Python", 15000, 30);
+		
+		
+		System.out.println(Course.getTaxRate());
+		
+		
 	}
 }
--- demo/src/oop1/TestSavingsAccount.java
+++ demo/src/oop1/TestSavingsAccount.java
@@ -1,44 +1,43 @@
 package oop1;
 
-class SavingsAccount  {
+class SavingsAccount {
 	private String acno, customer;
 	private double balance;
-	
-	public SavingsAccount(String ano, String cname) {
-		acno = ano;
-		customer = cname;
+
+	public SavingsAccount(String acno, String customer) {
+		this.acno = acno;
+		this.customer = customer;
 	}
 
-	public SavingsAccount(String ano, String cname, double bal) {
-		acno = ano;
-		customer = cname;
-		balance = bal;
+	public SavingsAccount(String acno, String customer, double balance) {
+		this(acno, customer); // call another constructor
+		this.balance = balance;
 	}
-	
-	
+
 	public void deposit(double amount) {
-		balance += amount;
+		this.balance += amount;
 	}
-	
+
 	public void withdraw(double amount) {
-		balance -= amount;
+		this.balance -= amount;
 	}
-	
+
 	public double getBalance() {
-		return balance;
+		return this.balance;
 	}
 }
+
 public class TestSavingsAccount {
 
 	public static void main(String[] args) {
-		 var s1 = new SavingsAccount("123", "Dave");
-	     s1.deposit(10000);
-	     s1.withdraw(2000);
-	     s1.deposit(5000);
-	     System.out.println(s1.getBalance());
-	     
-	     var s2 = new SavingsAccount("126", "Jack", 50000);
-	     System.out.println(s2.getBalance());
+		var s1 = new SavingsAccount("123", "Dave");
+		s1.deposit(10000);
+		s1.withdraw(2000);
+		s1.deposit(5000);
+		System.out.println(s1.getBalance());
+
+		var s2 = new SavingsAccount("126", "Jack", 50000);
+		System.out.println(s2.getBalance());
 
 	}
 
